2976994dc7f9d2bdd546e5af29698a3cfd77781d
SYLVAIN <akpalousylvain380@gmail.com>
2024-07-20 02:48:24 +0000

    le discipline grale

---
+++ script.js
@@ -0,0 +1,5 @@
+function buyNow() {
+    alert("Merci pour votre achat ! Vous allez être redirigé vers la page de paiement.");
+    // Redirection vers une page de paiement ou autre action
+    window.location.href = "t.me/Sylvain_akp"; // Exemple de redirection
+}
